2195ae06618664c0455d283f76cbc1e374836aec
Noha Shehab <nohashehab.iti@gmail.com>
2026-01-11 14:03:39 +0200

    intro to python

---
+++ intro.py
@@ -0,0 +1,48 @@
+""" Hello from python, welcome to session01 """
+
+name = "ahmed"
+# print(name)
+Name=  'Ali'
+
+num1 = 10
+nUm1  =20
+
+#if = 10
+
+#print("hello")
+
+# print(num1)
+# print = 500 # refer to int
+# print(num1)
+
+
+# name='ali' # IndentationError: unexpected indent
+
+
+# if(name=="ahmed") {console.log("hello")}
+
+name = 'Ahmed'
+if name=='ahmed':
+    print(name)
+    print("hello")
+else:
+    print("bye")
+
+
+bio = ('my name is noha'
+       'I works at iti')
+print(bio)
+
+bio2 = """my name is noha
+I works at iti"""
+
+'my name is noha\nI works at iti'
+
+print(bio2)
+
+#this is a comment
+
+"""
+this string is not attached to any variable
+ so it is considered to be a comment ??
+"""
